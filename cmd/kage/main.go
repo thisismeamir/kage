3f392995d8c165e96b694e1c06d5d969d90d7836
Kid A <thisismeamir@outlook.com>
2025-07-26 18:31:09 +0330

    [stash] testing database connection in main function. Do not stop at this commit.

--- cmd/kage/main.go
+++ cmd/kage/main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+	//i "github.com/thisismeamir/kage/internal/bootstrap"
+	//"github.com/thisismeamir/kage/internal/server"
+	//"log"
 	"fmt"
-	i "github.com/thisismeamir/kage/internal/init"
-	"github.com/thisismeamir/kage/internal/server"
-	"github.com/thisismeamir/kage/internal/watcher"
-	"log"
+	"github.com/thisismeamir/kage/internal/bootstrap/database"
 	"os"
 )
 
 func main() {
 
-	config := i.LoadConfiguration(i.GetConfigPath())
-	i.SetGlobalConfig(config)
-	serverAddr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
+	//config := i.LoadConfiguration(i.GetConfigPath())
+	//i.SetGlobalConfig(config)
+	//serverAddr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
 	art, _ := os.ReadFile("./util/asci-art")
 	fmt.Println(string(art))
-	log.Println("Starting Server in", serverAddr)
-	srv := server.New()
-	if err := srv.Start(serverAddr); err != nil {
-		log.Fatal("Failed to start server:", err)
+	var kageDB = &database.Database{}
+	kageDB, err := kageDB.OpenDatabase("./data/kage.sqlite")
+	if err != nil {
+		fmt.Println("Failed to open database:", err)
+		return
 	}
+	err = kageDB.CreateTable(database.TableSchema{
+		TableName: "atoms",
+		Schema: []database.ColumnSchema{
+			{Name: "id", Type: "INTEGER PRIMARY KEY AUTOINCREMENT"},
+			{Name: "name", Type: "TEXT NOT NULL"},
+			{Name: "path", Type: "TEXT NOT NULL"},
+		},
+	})
+	if err != nil {
+		fmt.Println("Failed to create table:", err)
+		return
+	}
+	fmt.Println("Database opened and table created successfully.")
+	fmt.Println(kageDB.GetAllTables())
+	a, _ := kageDB.GetTableSchema("atoms")
+	for _, col := range a {
+		fmt.Println(col.Name)
+	}
+
+	kageDB.DropTable("atoms")
+	kageDB.CloseDatabase()
+	//log.Println("Starting Server in", serverAddr)
+	//srv := server.New()
+	//if err := srv.Start(serverAddr); err != nil {
+	//	log.Fatal("Failed to start server:", err)
+	//}
 }

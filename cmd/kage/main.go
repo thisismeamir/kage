24b3dacf93e0e51f955e8a90e8ace9993ee1d519
Kid A <thisismeamir@outlook.com>
2025-07-27 01:28:14 +0330

    [refactor] Deleted Database implementation, basically we work with jsons for begin able to develop faster now.

--- cmd/kage/main.go
+++ cmd/kage/main.go
@@ -1,51 +1,41 @@
 package main
 
 import (
-	//i "github.com/thisismeamir/kage/internal/bootstrap"
-	//"github.com/thisismeamir/kage/internal/server"
-	//"log"
 	"fmt"
-	"github.com/thisismeamir/kage/internal/bootstrap/database"
+	"github.com/fsnotify/fsnotify"
+	i "github.com/thisismeamir/kage/internal/bootstrap"
+	"github.com/thisismeamir/kage/internal/server"
+	"github.com/thisismeamir/kage/internal/watcher"
+	"log"
 	"os"
 )
 
 func main() {
-
-	//config := i.LoadConfiguration(i.GetConfigPath())
-	//i.SetGlobalConfig(config)
-	//serverAddr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
+	config := i.LoadConfiguration(i.GetConfigPath())
+	i.SetGlobalConfig(config)
+	serverAddr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
 	art, _ := os.ReadFile("./util/asci-art")
 	fmt.Println(string(art))
-	var kageDB = &database.Database{}
-	kageDB, err := kageDB.OpenDatabase("./data/kage.sqlite")
-	if err != nil {
-		fmt.Println("Failed to open database:", err)
-		return
-	}
-	err = kageDB.CreateTable(database.TableSchema{
-		TableName: "atoms",
-		Schema: []database.ColumnSchema{
-			{Name: "id", Type: "INTEGER PRIMARY KEY AUTOINCREMENT"},
-			{Name: "name", Type: "TEXT NOT NULL"},
-			{Name: "path", Type: "TEXT NOT NULL"},
-		},
-	})
-	if err != nil {
-		fmt.Println("Failed to create table:", err)
-		return
-	}
-	fmt.Println("Database opened and table created successfully.")
-	fmt.Println(kageDB.GetAllTables())
-	a, _ := kageDB.GetTableSchema("atoms")
-	for _, col := range a {
-		fmt.Println(col.Name)
-	}
+	go watcher.WatchAtomPaths(func(event watcher.FileSystemEvent) {
+		// Checking if the change is related to .katom extensions:
+		if event.Path[len(event.Path)-6:] == ".katom" {
+			if event.Event == fsnotify.Create {
+				log.Println("New Atom Added")
+			} else if event.Event == fsnotify.Write {
+				log.Println("Atom Updated")
+			} else if event.Event == fsnotify.Remove {
+				log.Println("Atom Removed")
+			}
+		}
 
-	kageDB.DropTable("atoms")
-	kageDB.CloseDatabase()
-	//log.Println("Starting Server in", serverAddr)
-	//srv := server.New()
-	//if err := srv.Start(serverAddr); err != nil {
-	//	log.Fatal("Failed to start server:", err)
-	//}
+	})
+
+	go watcher.WatchModulePaths(func(event watcher.FileSystemEvent) {
+		log.Println("module event:", event)
+	})
+	log.Println("Starting Server in", serverAddr)
+	srv := server.New()
+	if err := srv.Start(serverAddr); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }

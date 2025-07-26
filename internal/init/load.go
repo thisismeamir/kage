fe09639854732aaa88a629fd5ff4efd296505f2c
Kid A <thisismeamir@outlook.com>
2025-07-26 16:44:35 +0330

    [implementation] global config file accessibility.

--- internal/init/load.go
+++ internal/init/load.go
@@ -31,3 +31,41 @@ func LoadConfiguration(path string) Config {
 	}
 	return cfg
 }
+
+var configPath = "./configs/default.conf.json"
+var globalConfig Config
+
+func SetConfigPath(path string) {
+	configPath = path
+}
+
+func GetConfigPath() string {
+	return configPath
+}
+
+func SetGlobalConfig(cfg Config) {
+	globalConfig = cfg
+}
+
+func GetGlobalConfig() Config {
+	return globalConfig
+}
+
+func SaveConfigFile() error {
+	file, err := os.Create(configPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data, err := json.MarshalIndent(globalConfig, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if _, err := file.Write(data); err != nil {
+		return err
+	}
+
+	return nil
+}

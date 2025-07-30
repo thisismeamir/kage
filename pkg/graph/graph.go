fab3a786b0065c1a98e3b184228b7737fcbbaf30
Kid A <thisismeamir@outlook.com>
2025-07-30 18:36:18 +0330

    [implementation] Methods for manipulating and saving/loading a graph added

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -3,6 +3,8 @@ package graph
 import (
 	"encoding/json"
 	"github.com/thisismeamir/kage/pkg/form"
+	"github.com/thisismeamir/kage/pkg/mapping"
+	"github.com/thisismeamir/kage/pkg/node"
 	"log"
 	"os"
 )
@@ -13,17 +15,84 @@ type Graph struct {
 	Metadata form.Metadata `json:"metadata"`
 }
 
-func LoadGraph(graphPath string) (*Graph, error) {
-	graph := &Graph{}
+func LoadGraph(graphPath string) (Graph, error) {
+	graph := Graph{}
 	data, err := os.ReadFile(graphPath)
 	if err != nil {
 		log.Fatalf("[ERROR] LoadGraph failed to read file: %s", err)
-		return nil, err
+		return Graph{}, err
 	} else {
 		if err := json.Unmarshal(data, graph); err != nil {
 			log.Fatalf("[ERROR] LoadGraph failed to unmarshal JSON: %s", err)
-			return nil, err
+			return Graph{}, err
 		}
 		return graph, nil
 	}
+}
+
+func (graph Graph) SaveGraph(path string) error {
+	data, err := json.MarshalIndent(graph, "", "  ")
+	if err != nil {
+		log.Printf("[ERROR] SaveGraph failed to save %s in marshal JSON %s: %s", graph.Identifier, path, err)
+		return err
+	} else if err := os.WriteFile(path, data, 0644); err != nil {
+		log.Printf("[ERROR] SaveGraph failed to save %s in path %s: %s", graph.Identifier, path, err)
+		return err
+	}
+	return nil
+}
+
+func (graph Graph) AddNodeToGraph(node node.Node) Graph {
+	graph.Model.Attachments.GraphNodes = append(graph.Model.Attachments.GraphNodes, node)
+	return graph
+}
+
+func (graph Graph) AddMapToGraph(mapp mapping.Map) Graph {
+	graph.Model.Attachments.GraphMaps = append(graph.Model.Attachments.GraphMaps, mapp)
+	return graph
+}
+
+func (graph Graph) AddGraphObject(object GraphObject) Graph {
+	graph.Model.GraphStructure = append(graph.Model.GraphStructure, object)
+	return graph
+}
+
+func (graph Graph) AddDependency(dep GraphDependency) Graph {
+	graph.Model.Dependencies = append(graph.Model.Dependencies, dep)
+	return graph
+}
+
+func (graph Graph) RemoveFromGraph(identifier string) Graph {
+	newGraphDependencies := make([]GraphDependency, 0)
+	newGraphAttachments := GraphAttachments{
+		make([]node.Node, 0),
+		make([]mapping.Map, 0),
+	}
+	for _, existingNode := range graph.Model.Attachments.GraphNodes {
+		if existingNode.Identifier != identifier {
+			newGraphAttachments.GraphNodes = append(newGraphAttachments.GraphNodes, existingNode)
+		}
+	}
+	for _, existingMap := range graph.Model.Attachments.GraphMaps {
+		if existingMap.Identifier != identifier {
+			newGraphAttachments.GraphMaps = append(newGraphAttachments.GraphMaps, existingMap)
+		}
+	}
+	for _, dependency := range graph.Model.Dependencies {
+		if dependency.Identifier != identifier {
+			newGraphDependencies = append(newGraphDependencies, dependency)
+		}
+	}
+	newModel := GraphModel{
+		graph.Model.Execution,
+		newGraphDependencies,
+		graph.Model.GraphStructure,
+		newGraphAttachments,
+	}
+	newGraph := Graph{
+		graph.Form,
+		newModel,
+		graph.Metadata,
+	}
+	return newGraph
 }

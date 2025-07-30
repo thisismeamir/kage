73095f3a480aeb03f1f0a72921b4c0cbfddcd34f
Kid A <thisismeamir@outlook.com>
2025-07-30 18:38:27 +0330

    [implementation] node, map, and graph registries implemented.

---
+++ internal/internal-pkg/registry/GraphRegistry.go
@@ -0,0 +1,65 @@
+package registry
+
+import (
+	"encoding/json"
+	i "github.com/thisismeamir/kage/internal/bootstrap/config"
+	"github.com/thisismeamir/kage/pkg/graph"
+	"log"
+	"os"
+)
+
+type GraphRegistry struct {
+	Graphs []GraphRegister `json:"graphs"`
+}
+
+type GraphRegister struct {
+	Identifier string `json:"identifier"`
+	Path       string `json:"path"`
+}
+
+func LoadGraphRegistry(registryPath string) (GraphRegistry, error) {
+	data, err := os.ReadFile(registryPath)
+	if err != nil {
+		log.Fatalf("[FATAL] Could not load graph registry JSON: %s", err)
+		return GraphRegistry{}, err
+	} else {
+		var registry GraphRegistry
+		if err := json.Unmarshal(data, &registry); err != nil {
+			log.Fatalf("[FATAL] Error unmarshalling graph registry JSON: %s", err)
+			return GraphRegistry{}, err
+		}
+		return registry, nil
+	}
+}
+
+func (registry GraphRegistry) SaveGraphRegistry() GraphRegistry {
+	registryPath := i.GetGlobalConfig().BasePath + "/data/graph.registry.json"
+	data, err := json.Marshal(registry)
+	if err != nil {
+		log.Fatalf("[FATAL] Error marshalling graph registry JSON: %s", err)
+	} else if err := os.WriteFile(registryPath, data, 0644); err != nil {
+		log.Fatalf("[FATAL] Could not save graph registry JSON: %s", err)
+	}
+	return registry
+}
+
+func (registry GraphRegistry) AddGraphToRegistry(graph graph.Graph) GraphRegistry {
+	registry.Graphs = append(registry.Graphs, GraphRegister{
+		Identifier: graph.Identifier,
+		Path:       graph.Path,
+	})
+
+	return registry
+}
+
+func (registry GraphRegistry) RemoveGraphFromRegistry(identifier string) GraphRegistry {
+	newRegistry := GraphRegistry{
+		Graphs: []GraphRegister{},
+	}
+	for _, graph := range registry.Graphs {
+		if graph.Identifier != identifier {
+			newRegistry.Graphs = append(newRegistry.Graphs, graph)
+		}
+	}
+	return newRegistry
+}
---
+++ internal/internal-pkg/registry/NodeRegistry.go
@@ -0,0 +1,81 @@
+package registry
+
+import (
+	"encoding/json"
+	i "github.com/thisismeamir/kage/internal/bootstrap/config"
+	"github.com/thisismeamir/kage/pkg/node"
+	"log"
+	"os"
+)
+
+type NodeRegistry struct {
+	Nodes []NodeRegister `json:"nodes"`
+}
+
+type NodeRegister struct {
+	Identifier   string                 `json:"identifier"`
+	Path         string                 `json:"path"`
+	InputSchema  map[string]interface{} `json:"input_schema"`
+	OutputSchema map[string]interface{} `json:"output_schema"`
+}
+
+func LoadNodeRegistry(registryPath string) (NodeRegistry, error) {
+	data, err := os.ReadFile(registryPath)
+	if err != nil {
+		log.Fatalf("[FATAL] Could not load node registry JSON: %s", err)
+		return NodeRegistry{}, err
+	} else {
+		var nodeRegistry NodeRegistry
+		if err := json.Unmarshal(data, &nodeRegistry); err != nil {
+			log.Fatalf("[FATAL] Error unmarshalling node registry JSON: %s", err)
+			return NodeRegistry{}, err
+		}
+		return nodeRegistry, nil
+	}
+}
+
+func (registry NodeRegistry) SaveNodeRegistry() NodeRegistry {
+	registryPath := i.GetGlobalConfig().BasePath + "/data/node.registry.json"
+	data, err := json.Marshal(registry)
+	if err != nil {
+		log.Fatalf("[FATAL] Error marshalling node registry JSON: %s", err)
+	} else if err := os.WriteFile(registryPath, data, 0755); err != nil {
+		log.Fatalf("[FATAL] Error saving node registry JSON: %s", err)
+	}
+	return registry
+}
+
+func (registry NodeRegistry) NodeExists(identifier string) bool {
+	for _, node := range registry.Nodes {
+		if node.Identifier == identifier {
+			return true
+		}
+	}
+	return false
+}
+
+func (registry NodeRegistry) AddNodeToRegistry(node node.Node) NodeRegistry {
+	if !registry.NodeExists(node.Identifier) {
+		registry.Nodes = append(registry.Nodes, NodeRegister{
+			Identifier:   node.Identifier,
+			Path:         node.Path,
+			InputSchema:  node.Model.ExecutionModel.InputSchema,
+			OutputSchema: node.Model.ExecutionModel.OutputSchema,
+		})
+	}
+	return registry
+}
+
+func (registry NodeRegistry) RemoveNodeFromRegistry(identifier string) NodeRegistry {
+	newRegistry := NodeRegistry{
+		Nodes: make([]NodeRegister, 0),
+	}
+	for _, r := range registry.Nodes {
+		if r.Identifier != identifier {
+			newRegistry.Nodes = append(newRegistry.Nodes, r)
+		} else {
+			node.LoadNode(r.Path)
+		}
+	}
+	return newRegistry
+}

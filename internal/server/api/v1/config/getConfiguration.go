4f0571c93e4d70089a4984ee3fe4726460441393
Kid A <thisismeamir@outlook.com>
2025-07-30 18:37:09 +0330

    [bug/fixed] configuration in getConfiguration.go

--- internal/server/api/v1/config/getConfiguration.go
+++ internal/server/api/v1/config/getConfiguration.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"github.com/gin-gonic/gin"
-	i "github.com/thisismeamir/kage/internal/bootstrap"
+	i "github.com/thisismeamir/kage/internal/bootstrap/config"
 )
 
 func GetConfiguration(c *gin.Context) {

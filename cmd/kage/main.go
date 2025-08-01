8d79c3b5e7e51d45b26f389e724d58dcf5d0770b
Kid A <thisismeamir@outlook.com>
2025-08-02 00:04:04 +0330

    [implementation] init function for initialization.

--- cmd/kage/main.go
+++ cmd/kage/main.go
@@ -2,27 +2,59 @@ package main
 
 import (
 	"fmt"
-	i "github.com/thisismeamir/kage/internal/bootstrap"
-	config2 "github.com/thisismeamir/kage/internal/bootstrap/config"
-	. "github.com/thisismeamir/kage/internal/runtime/handler/file"
-	. "github.com/thisismeamir/kage/internal/server"
+	"github.com/thisismeamir/kage/internal/bootstrap/init_methods"
 	"log"
 	"os"
 )
 
-func main() {
+var serverAddr string
+var serverPort string
+var apiPath string
+var apiVersion string
+
+func init() {
+	// ASCII ART FOR RUNNING THE PROGRAM IN CLI.
 	art, _ := os.ReadFile("./util/asci-art")
 	fmt.Println(string(art))
-	// Initialization Function:
-	config := config2.LoadConfiguration(config2.GetConfigPath())
-	config2.SetGlobalConfig(config)
-	serverAddr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
-	serverAddr, config := i.InitKage()
-
-	// Start the server
-	log.Println("Starting Server in", serverAddr)
-	srv := New()
-	if err := srv.Start(serverAddr); err != nil {
-		log.Fatal("Failed to start server:", err)
+	// Loading Configuration
+	config, err := init_methods.LoadConfiguration("/home/kid-a/projects/kage/configs/default.conf.json")
+	if err == nil {
+		err := init_methods.SetupBasePath(config)
+		if err != nil {
+			log.Fatalf("[FATAL] Unable to set up base path, and directories. %s", err)
+		}
+	} else {
+		log.Fatalf("[FATAL] Unable to load configuration. %s", err)
 	}
+
+	// initializing logger
+	init_methods.InitializeLogger(config.BasePath + config.Logging.File)
+
+	files := init_methods.GetPathsObjects(config.Paths)
+	for _, file := range files {
+		log.Println(file)
+		value := init_methods.GetTypeOfObject(file)
+		println(value)
+	}
+}
+
+func main() {
+
+	// Initialization Function:
+	//init_methods := config2.LoadConfiguration(config2.GetConfigPath())
+	//config2.SetGlobalConfig(init_methods)
+	//
+	//serverAddr, init_methods, watchers := i.InitKage()
+	//
+	//for _, watcher := range watchers {
+	//	go func() {
+	//		_ = watcher.Start()
+	//	}()
+	//}
+	//// Start the server
+	//log.Println("Starting Server in", serverAddr)
+	//srv := New()
+	//if err := srv.Start(serverAddr); err != nil {
+	//	log.Fatal("Failed to start server:", err)
+	//}
 }

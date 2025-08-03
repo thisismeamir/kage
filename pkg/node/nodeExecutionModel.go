e2c7aea7e9f46ac867f2e49f751ea559ae27bb31
Kid A <thisismeamir@outlook.com>
2025-08-03 18:24:31 +0330

    [implementation] node structure changed

--- pkg/node/nodeExecutionModel.go
+++ pkg/node/nodeExecutionModel.go
@@ -4,4 +4,5 @@ type ExecutionModel struct {
 	Language     string                 `json:"language"`
 	InputSchema  map[string]interface{} `json:"input_schema"`
 	OutputSchema map[string]interface{} `json:"output_schema"`
+	Artifacts    []string               `json:"artifacts"`
 }

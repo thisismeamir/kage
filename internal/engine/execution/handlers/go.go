1c19a42cc77a00527b12c033bdebb3e2330761ce
Kid A <thisismeamir@outlook.com>
2025-08-03 18:28:18 +0330

    [deletion] un-necessary files

--- internal/engine/execution/handlers/go.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-type GoHandler struct{}
-
-func (g *GoHandler) Run(source string, input map[string]interface{}) (map[string]interface{}, error) {
-	// Implementation for running Go code
-	return nil, nil // Placeholder return
-}
--- internal/engine/execution/handlers/python.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-type PythonHandler struct{}
-
-func (p *PythonHandler) Run(source string, input map[string]interface{}) (map[string]interface{}, error) {
-	// Implementation for running Python code
-	return nil, nil // Placeholder return
-}

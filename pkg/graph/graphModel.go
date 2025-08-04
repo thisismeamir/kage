2e55d12fabde6c66ce5a67309b2490424626449b
Kid A <thisismeamir@outlook.com>
2025-08-04 18:42:15 +0330

    [refactor] Removed attachements and dependencies previously. Some modules were still behind

--- pkg/graph/graphModel.go
+++ pkg/graph/graphModel.go
@@ -1,8 +1,6 @@
 package graph
 
 type GraphModel struct {
-	Execution    GraphExecutionModel `json:"execution"`
-	Dependencies []GraphDependency   `json:"dependencies,omitempty"`
-	Structure    []GraphObject       `json:"structure"`
-	Attachments  GraphAttachments    `json:"attachments,omitempty"`
+	Execution GraphExecutionModel `json:"execution,omitempty"`
+	Structure []GraphObject       `json:"structure"`
 }

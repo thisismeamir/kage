53f784f2d346451017fe900c8a63a2721ff7e20a
Kid A <thisismeamir@outlook.com>
2025-08-09 03:37:15 +0330

    [stash]

--- cmd/kage/main.go
+++ cmd/kage/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
 	"github.com/thisismeamir/kage/internal/bootstrap/init_methods"
-	"github.com/thisismeamir/kage/internal/engine/scheduler"
+	engine2 "github.com/thisismeamir/kage/internal/engine"
+	execution_system "github.com/thisismeamir/kage/internal/engine/execution-system"
+	system_monitor "github.com/thisismeamir/kage/internal/engine/system-monitor"
 	task_manager "github.com/thisismeamir/kage/internal/engine/task-manager"
 	"github.com/thisismeamir/kage/internal/internal-pkg/config"
-	"github.com/thisismeamir/kage/internal/internal-pkg/event"
 	"github.com/thisismeamir/kage/internal/internal-pkg/registry"
-	"github.com/thisismeamir/kage/internal/server"
 	"github.com/thisismeamir/kage/internal/watcher"
+	"github.com/thisismeamir/kage/util"
 	"log"
 	"os"
-	"time"
+	"sync"
 )
 
 var serverAddr string
@@ -60,20 +62,6 @@ func init() {
 	if err := w.Start(); err != nil {
 		log.Fatalf("[FATAL] Watcher failed to start: %v", err)
 	}
-	// Watcher for Task Manager to know about changes in flows directory
-	w2, err := watcher.NewWatcher([]string{conf.BasePath + "/tmp/flows"}, func(event watcher.FileSystemEvent) {
-		if event.Event == fsnotify.Create || event.Event == fsnotify.Remove {
-			log.Println("[INFO] Task Manager detected a change in flows directory, re-initializing Task Manager.")
-			task_manager.InitializeTaskManager(conf)
-		}
-
-	})
-	if err != nil {
-		log.Fatalf("[FATAL] Unable to start watcher: %v", err)
-	}
-	if err := w2.Start(); err != nil {
-		log.Fatalf("[FATAL] Watcher failed to start: %v", err)
-	}
 
 	// setting up global values:
 	serverAddr = fmt.Sprintf("%s:%d", conf.Server.Host, conf.Server.Port)
@@ -91,29 +79,37 @@ func init() {
 	}
 	endArt, _ := os.ReadFile("./util/main-banner")
 	fmt.Println("\n" + string(endArt))
+
 }
 
 func main() {
 	reg, _ = registry.LoadRegistry(conf.BasePath + "/data/registry.json")
-
-	tm := task_manager.InitializeTaskManager(conf)
-	fmt.Printf("%v ", tm)
-
-	//for i := 0; i < 20; i++ {
-	//	e := event.Event{
-	//		Graph:   "Sample-Graph.0.0.1.graph",
-	//		Date:    time.Now().Format("2006-01-02-15-04-.000000"),
-	//		Urgency: i % 4,
-	//	}
-	//	e = e.GenerateIdentifier()
-	//	f := scheduler.Scheduler(e, *reg, conf)
-	//	log.Printf("Scheduler initialized with event: %v\n", f)
-	//}
-
-	//Start the server
-	log.Println("Starting Server in", serverAddr)
-	srv := server.New(clientAddr)
-	if err := srv.Start(serverAddr); err != nil {
-		log.Fatal("Failed to start server:", err)
+	sm := system_monitor.NewSystemMonitor()
+	ex := execution_system.NewExecutionSystem()
+	newEvent := task_manager.Event{
+		Identifier:      task_manager.IdentifierGeneration("event"),
+		GraphIdentifier: "Sample-Graph.0.0.1.graph",
+		Urgency:         1,
+		Input:           util.LoadJson("/home/kid-a/kage/first/sample_input.json"),
 	}
+
+	newEvent.ScheduleFlow(conf, *reg)
+	// Create a new Engine
+	engine := engine2.NewEngine(conf, *reg, *ex, *sm)
+
+	// Create context and wait group for concurrency control
+	ctx, _ := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go engine.Routine(ctx, &wg) // Run the engine's routine in the background
+
+	//// For example, if you want to stop it after 30 seconds:
+	//time.Sleep(30 * time.Second)
+	//cancel() // This will stop the engine
+
+	// Wait for the goroutine to complete before shutting down
+	wg.Wait()
+	log.Println("Engine stopped")
+
 }
